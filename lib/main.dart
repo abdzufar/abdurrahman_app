39150e4a42944ad1b30573d19f7efbf03eae3dac
abdzufar <abdzufar@student.uns.ac.id>
2022-03-18 14:30:59 +0700

    Changed the comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 // Copyright 2018 The Flutter team. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// found in the LICENSE file. testing
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
